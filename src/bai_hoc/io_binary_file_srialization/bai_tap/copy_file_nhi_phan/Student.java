52d648c9016dc66c136e48e65e374407e5e89379
nguyentienhai130801 <tienhai19631959@gmail.com>
2020-07-23 08:21:25 +0700

    "hello"

---
+++ src/bai_hoc/io_binary_file_srialization/bai_tap/copy_file_nhi_phan/Student.java
@@ -0,0 +1,32 @@
+package bai_hoc.io_binary_file_srialization.bai_tap.copy_file_nhi_phan;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
