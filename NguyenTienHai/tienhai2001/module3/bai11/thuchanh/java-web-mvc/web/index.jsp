be19f4daf72264ff7acc46510ca3394871ece669
nguyentienhai130801 <tienhai19631959@gmail.com>
2020-09-01 10:08:15 +0700

    bailam

--- NguyenTienHai/tienhai2001/module3/bai11/thuchanh/java-web-mvc/web/index.jsp
+++ NguyenTienHai/tienhai2001/module3/bai11/thuchanh/java-web-mvc/web/index.jsp
@@ -6,12 +6,12 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
+  <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+  <html>
   <head>
-    <title>$Title$</title>
+  <title>$Title$</title>
   </head>
   <body>
   <c:redirect url="/customers"></c:redirect>
   </body>
-</html>
+  </html>
---
+++ NguyenTienHai/tienhai2001/module3/bai12/baitap/user_manager/src/com/user_manager/bo/IUserBO.java
@@ -0,0 +1,22 @@
+package com.user_manager.bo;
+
+import com.user_manager.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
+
+public interface IUserBO {
+    void insertUser(User user) throws SQLException;
+
+     User selectUser(int id);
+
+     List<User> selectAllUsers();
+
+     boolean deleteUser(int id) throws SQLException;
+
+   boolean updateUser(User user) throws SQLException;
+
+   List<User> getUserByCountry(String country);
+    List<User> sortByName();
+}
